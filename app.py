18a637fc1de64142f4a7674f3b2fbc2f0a2d2005
sahaj9 <sahajjain739739@gmail.com>
2023-06-08 22:57:06 +0530

    Added marshmallow for data validation of apis

--- app.py
+++ app.py
@@ -1,49 +1,17 @@
-from flask import Flask, request
-import uuid
-from db import items
+from flask import Flask
+from resources.item import blp as ItemBluePrint
+from flask_smorest import Api
+
 app = Flask(__name__)
 
-@app.get('/item')
-def get_item():
-    id = request.args.get('id')
-    if id is None:
-        return {"items":items}
-    try :
-        return items[id]
-    except KeyError:
-        return {'message': "Recoed doesn't exisit"}, 404
+app.config["PROPAGATE_EXCEPTIONS"] = True
+app.config["API_TITLE"] = "Stores Rest APIs"
+app.config["API_VERSION"] = "v1"
+app.config["OPENAPI_VERSION"] = "4.0.0"
+app.config["OPENAPI_URL_PREFIX"] = "/"
+app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger"
+app.config["OPENAPI_SWAGGER_UI_URI"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
-@app.post('/item')
-def add_item():
-    request_Data = request.get_json()
-    if "name" not in request_Data or "price" not in request_Data:
-        return {'message': "Please input name and price must be included in body"}, 400
-    items[uuid.uuid4().hex]= request.get_json()
-    return {"message":"item added successfully"}, 201
+api = Api(app)
 
-@app.put('/item')
-def update_item():
-    id = request.args.get('id')
-    if id == None:
-        return {'message': "Given id not found"}, 404 
-
-    if id in items.keys():
-        request_Data = request.get_json()
-        if "name" not in request_Data or "price" not in request_Data:
-            return {'message': "Please input name and price must be included in body"}, 400 
-        items[id] = request.get_json()
-        return {"message":"item updated successfully"}, 200
-    else :
-         return {'message': "Not found"}, 404
-
-
-@app.delete('/item')
-def delete_item():
-    id = request.args.get('id')
-    if id == None:
-        return {'message': "Given id not found"}, 404 
-    if id in items.keys():
-        del items[id] 
-        return {'message' : 'Item delete successfully'}
-    return {'message': "Recoed doesn't exisit"}, 404
-
+api.register_blueprint(ItemBluePrint)
--- resources/item.py
+++ resources/item.py
@@ -1,3 +1,47 @@
 from flask import Flask, request
 import uuid
 from db import items
+from flask.views import MethodView
+from flask_smorest import Blueprint
+from schemas import ItemSchema
+
+
+blp = Blueprint("items", __name__, description="Operations on items")
+
+@blp.route("/item")
+class Item(MethodView):
+    def get(self):
+        id = request.args.get('id')
+        if id is None:
+            return {"items":items}
+        try :
+            return items[id]
+        except KeyError:
+            return {'message': "Recoed doesn't exisit"}, 404
+
+    @blp.arguments(ItemSchema)
+    def post(self, request_data):
+        items[uuid.uuid4().hex]= request_data
+        return {"message":"item added successfully"}, 201
+
+    @blp.arguments(ItemSchema)
+    def put(self, request_data):
+        id = request.args.get('id')
+        if id == None:
+            return {'message': "Given id not found"}, 404 
+
+        if id in items.keys():
+            items[id] = request_data
+            return {"message":"item updated successfully"}, 200
+        else :
+            return {'message': "Not found"}, 404
+        
+
+    def delete(self):
+        id = request.args.get('id')
+        if id == None:
+            return {'message': "Given id not found"}, 404 
+            if id in items.keys():
+                del items[id] 
+                return {'message' : 'Item delete successfully'}
+        return {'message': "Recoed doesn't exisit"}, 404
